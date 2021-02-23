39814dd0a91382a886b95dd71eb30c3566a0f949
Abraham <abrahamjosuehdez14@gmail.com>
2021-02-22 21:20:30 -0800

    Add time row action with jQuery

--- js/my.js
+++ js/my.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-    let date = new Date();
+    let date = new Date("February 22, 2021 07:24:00");
     let day = date.getDay();
     let hour = date.getHours();
 
+    // Dark the day column header area
     if (day === 1) {
         $("th#monday").addClass("active-day");
     }
@@ -17,5 +18,22 @@ $(document).ready(function(){
     }
     else if (day === 5) {
         $("th#friday").addClass("active-day");
+    }
+
+    // Dark the time row data area
+    if (hour == 7) {
+        $("td#class-time1").addClass("active-day")
+    }
+    else if (hour == 8) {
+        $("td#class-time2").addClass("active-day")
+    }
+    else if (hour == 9) {
+        $("td#class-time3").addClass("active-day")
+    }
+    else if (hour == 10) {
+        $("td#class-time4").addClass("active-day")
+    }
+    else if (hour == 11) {
+        $("td#class-time5").addClass("active-day")
     }
 });
